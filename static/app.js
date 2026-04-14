27cd691219496651df6e60acd2d53be5c721998b
2eliot <yorbitub@gmail.com>
2026-04-14 17:58:33 -0400

    Fix package button selected state

--- static/app.js
+++ static/app.js
@@ -205,10 +205,9 @@ function setupDashboard() {
 
         packageList.innerHTML = "";
         activeService.packages.forEach((pkg) => {
-            const cartQuantity = state.cart.filter((item) => item.packageId === pkg.id).length;
             const button = document.createElement("button");
             button.type = "button";
-            button.className = `package-button${cartQuantity ? " is-selected" : ""}`;
+            button.className = "package-button";
             button.innerHTML = `<span class="package-button__name">${escapeHtml(pkg.name)}</span><strong>${escapeHtml(pkg.display_price)}</strong>`;
             button.addEventListener("click", () => addPackage(pkg, activeService, button));
             packageList.appendChild(button);
@@ -225,7 +224,6 @@ function setupDashboard() {
             displayPrice: pkg.display_price,
         });
         if (button) {
-            button.classList.add("is-selected");
             button.classList.remove("is-just-added");
             window.requestAnimationFrame(() => button.classList.add("is-just-added"));
             window.setTimeout(() => button.classList.remove("is-just-added"), 260);
